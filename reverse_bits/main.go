67df791e2003f2924c323248b36026e85e35df0f
yangchengkai1 <1351599172@qq.com>
2019-08-28 19:16:41 +0800

    Update main.go

--- reverse_bits/main.go
+++ reverse_bits/main.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func main() {
 	num := uint32(439329)
-	fmt.Printf("num = %032b,reverse = %b\n", num, reverseBits(num))
+	fmt.Printf("%032b\n", num)
+	fmt.Printf("%b\n", reverseBits(num))
+	fmt.Printf("%b\n", reverseBitsFirst(num))
 }
 
 func reverseBits(num uint32) uint32 {
@@ -15,5 +17,26 @@ func reverseBits(num uint32) uint32 {
 		num >>= 1
 		count--
 	}
+	return bits
+}
+
+// Another version
+func reverseBitsFirst(num uint32) uint32 {
+	bits, count := uint32(0), uint32(0)
+	mask := uint32(2147483648)
+
+	for count < 32 {
+		mm := num & mask
+		if mm == 0 {
+			count++
+			mask >>= 1
+			continue
+		}
+
+		bits |= 1 << count
+		count++
+		mask >>= 1
+	}
+
 	return bits
 }
