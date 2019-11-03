881d454a843fb12715ba99531a230f1747b8c48a
yangchengkai1 <1351599172@qq.com>
2019-11-03 21:27:49 +0800

    Create main.go

---
+++ find_median_sorted_arrays/main.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numsFirst = []int{1, 2, 4}
+	var numsSecond = []int{3, 8}
+	fmt.Println(findMedianSortedArrays(numsFirst, numsSecond))
+}
+
+func findMedianSortedArrays(numsFirst []int, numsSecond []int) float64 {
+	nums := combine(numsFirst, numsSecond)
+	return medianOf(nums)
+}
+
+func combine(mis, njs []int) []int {
+	lenMis, i := len(mis), 0
+	lenNjs, j := len(njs), 0
+	res := make([]int, lenMis+lenNjs)
+
+	for k := 0; k < lenMis+lenNjs; k++ {
+		if i == lenMis ||
+			(i < lenMis && j < lenNjs && mis[i] > njs[j]) {
+			res[k] = njs[j]
+			j++
+			continue
+		}
+
+		if j == lenNjs ||
+			(i < lenMis && j < lenNjs && mis[i] <= njs[j]) {
+			res[k] = mis[i]
+			i++
+		}
+	}
+
+	return res
+}
+
+func medianOf(nums []int) float64 {
+	l := len(nums)
+
+	if l == 0 {
+		panic("切片的长度为0，无法求解中位数。")
+	}
+
+	if l%2 == 0 {
+		return float64(nums[l/2]+nums[l/2-1]) / 2.0
+	}
+
+	return float64(nums[l/2])
+}
